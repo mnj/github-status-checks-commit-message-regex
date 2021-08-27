d948fc252d0c37a46de734c25e658e0e495311b2
Michael N. Jensen <michael.jensen@dustin.dk>
2021-08-27 13:12:56 +0200

    Add extra options for what to check, and reorganize the code

--- README.md
+++ README.md
@@ -4,7 +4,19 @@ A simple python scripts, that checks if the commit message in the pull request m
 
 Just requires a plain Github workflow to run, on ubuntu-latest (or anything that has python3)
 
-## Example status check Github Action workflow
+There is a few options you can specify:
+| Input                 | Description                                                   | Required? | Default |
+|-----------------------|---------------------------------------------------------------|-----------|---------|
+| pattern               | The regex pattern to use, to check for a valid commit message | Yes       | N/A     |
+| check_commit_title    | Check for the pattern in the commit title (first line)        | No        | false   |
+| check_commit_body     | Check for thee pattern in the rest of the commit message      | No        | false   |
+| check_commit_message  | Check for the pattern in the full commit message (title+body) | No        | true    |
+| check_all_commits     | Check all individual commit message in the pull request       | No        | false   |
+| github_token          | The GITHUB_TOKEN to use for authenticating to github          | No        | N/A     |
+
+Using check_all_commits, requires you to pass the {{ secrets.GITHUB_TOKEN }}, so it has access to all the commits in the pull request
+
+## Example status check Github Action workflow simple
 ```yml
 name: Check commit message
 on: [pull_request]
@@ -18,6 +30,28 @@ jobs:
       uses: mnj/github-status-checks-commit-message-regex@v1
       with:
         pattern: '([A-Z][A-Z0-9]+-[0-9]+)' # Match all upper casee Jira ticket ids
+```
+
+## Example status check Github Action workflow, full example
+```yml
+name: Check commit message
+on: [pull_request]
+
+jobs:
+  check-commit-message:
+  name: Check commit message
+  runs-on: ubuntu-latest
+  steps:
+    - name: Check commit message
+      uses: mnj/github-status-checks-commit-message-regex@v1
+      with:
+        pattern: '([A-Z][A-Z0-9]+-[0-9]+)' # Match all upper casee Jira ticket ids
+        check_commit_title: 'false'
+        check_commit_body: 'false'
+        check_commit_message: 'true'
+        check_all_commits: 'true'
+
+        github_token: ${{ secrets.GITHUB_TOKEN }}
 ```
 
 ## License
--- main.py
+++ main.py
@@ -1,49 +1,115 @@
+##### Imports #####
 import re
 import sys
 import os
 import json
+from distutils.util import strtobool
+# import Github
 
-re_match_title = None
-re_match_body = None
+# Script args:
+# sys.argv[1] - Pattern (string)
+# sys.argv[2] - Check commit title (bool)
+# sys.argv[3] - Check commit body (bool)
+# sys.argv[5] - Check commit message (bool)
+# sys.argv[6] - Check all commits (bool)
+# sys.argv[7] - Github token (string)
 
-try:
-    # Try to open the event file (json) that is specified with the environment
-    # varible: GITHUB_EVENT_PATH, this will contain the pull request details
-    # but it will not list each individual commit message, for that we need to
-    # use the python github client, with required the GITHUB_TOKEN to work.
+# Check if the argument exists, (and convert them to Python booleans), 
+# if they are not present, then set them to some sane defaults
+pattern = sys.argv[1] if (len(sys.argv) >= 2) else None
+check_commit_title = bool(strtobool(sys.argv[2])) if len(sys.argv) >= 3 else False
+check_commit_body = bool(strtobool(sys.argv[3])) if len(sys.argv) >= 4 else False
+check_commit_message = bool(strtobool(sys.argv[4])) if len(sys.argv) >= 5 else True
+check_all_commits = bool(strtobool(sys.argv[5])) if len(sys.argv) >= 6 else False
+github_token = sys.argv[6] if len(sys.argv) >= 7 else None
 
-    # Check if the GITHUB_EVENT_PATH environment variable has been set
-    if "GITHUB_EVENT_PATH" in os.environ:
+##### Functions #####
+def check_valid_title(pull_request_json):
+    # Check if there is a title element (first line of the git commit message)
+    if "title" in pull_request_json:
+        # Check that it's not set to null/empty
+        if pull_request_json['title'] is not None:
+            regex_match = re.search(pattern, pull_request_json['title'])
+            return regex_match
+        else:
+            return False # Didn't contain anything/exist, so cant be valid
+    else:
+        raise SystemExit('Did not find the expected title elemment in the pull request event data!')
 
-        # Open the event json file, and parse it with json
-        with open(os.environ['GITHUB_EVENT_PATH'], 'r') as event_file:
-            event_data = json.load(event_file)
-            
-            # Check that we are actually running on a pull request status check
-            if "pull_request" in event_data:
+def check_valid_body(pull_request_json):
+    # Check if there is a body element (the rest of the commit message, excl the first line)
+    if "body" in pull_request_json:
+        # Check that it's not set to null/empty
+        if pull_request_json['body'] is not None:
+            regex_match = re.search(pattern, pull_request_json['body'])
+            return regex_match
+        else:
+            return False # Didn't contain anything/exist, so cant be valid
+    else:
+        raise SystemExit('Did not find the expected body elemment in the pull request event data!')
 
-                # Check if there is a title element (first line of the git commit message)
-                if "title" in event_data['pull_request']:
+def check_valid_message(pull_request_json):
+    full_message = ""
+    
+    if "title" in pull_request_json:
+        if pull_request_json['title'] is not None:
+            full_message += pull_request_json['title']
+    if "body" in pull_request_json:
+        if pull_request_json['body'] is not None:
+            full_message += "\n" + pull_request_json['body']
 
-                    # Check that it's not set to null/empty
-                    if event_data['pull_request']['title'] is not None:
-                    
-                        # Get the match result, for the regex search, based on the pattern in argv[1]
-                        re_match_title = re.search(sys.argv[1], event_data['pull_request']['title'])
+    if full_message == "":
+        raise SystemExit('Both the title and body of the commit message can not be empty!')
+    else:
+        return re.search(pattern, full_message)
 
-                # Check if there is a body element (the rest of the commit message, excl the first line)
-                if "body" in event_data['pull_request']:
+def get_pull_request_json():
+    try:
+        # Try to open the event file (json) that is specified with the environment
+        # varible: GITHUB_EVENT_PATH, this will contain the pull request details
+        # but it will not list each individual commit message, for that we need to
+        # use the python github client, with required the GITHUB_TOKEN to work.
 
-                    # Check that it's not set to null/empty
-                    if event_data['pull_request']['body'] is not None:
+        # Check if the GITHUB_EVENT_PATH environment variable has been set
+        if "GITHUB_EVENT_PATH" in os.environ:
 
-                        # Get the match result, for the regex search, based on the pattern in argv[1]
-                        re_match_body = re.search(sys.argv[1], event_data['pull_request']['body'])
-except:
-    raise SystemExit('Unable to parse the pull request!')
+            # Open the event json file, and parse it with json
+            with open(os.environ['GITHUB_EVENT_PATH'], 'r') as event_file:
+                event_data = json.load(event_file)
+                
+                # Check that we are actually running on a pull request status check
+                if "pull_request" in event_data:
+                    return event_data['pull_request']
+                else:
+                    raise SystemExit('There was no pull request data in the triggered event!')
+        else:
+            raise SystemExit('Missing the GITHUB_EVENT_PATH environment variable!')
+    except:
+        raise SystemExit('Unable to parse the pull request!')
 
-# Return success if there was a regex match in either the title or body or both
-if (re_match_title is not None) or (re_match_body is not None):
-    sys.exit(0)
-else:
-    raise SystemExit('You did not specify a Jira ticket number in this pull request!')
+##### Main #####
+if __name__ == '__main__':
+    valid_pull_request = True # Assume true, unless checks fail
+
+    pull_request_json = get_pull_request_json()
+    
+    if check_all_commits:
+        pass
+    else:
+        # We can rely on just the pull request json without having to use PyGithub
+        if check_commit_title:
+            if not check_valid_title(pull_request_json):
+                valid_pull_request = False
+
+        if check_commit_body:
+            if not check_valid_body(pull_request_json):
+                valid_pull_request = False
+
+        if check_commit_message:
+            if not check_valid_message(pull_request_json):
+                valid_pull_request = False
+
+    if valid_pull_request:
+        sys.exit(0)
+    else:
+        raise SystemExit('You did not specify a Jira ticket number in this pull request!')
